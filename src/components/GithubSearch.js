859903762ac98e929be2d2ec2b53af6a0672f80a
Ryan Pool <rlpool06@gmail.com>
2021-07-11 23:49:05 -0500

    CSS Update

--- src/components/GithubSearch.js
+++ src/components/GithubSearch.js
@@ -8,22 +8,24 @@ class GithubSearch extends Component {
         this.state = {
             username: '',
             repos: [],
-            apiMsg:'',
             userInfo: {}
         }
     };
 
     handleChange = (e) => {
         this.setState({ username: e.target.value });
-        setTimeout(this.handleSubmit, 1000)
     }
 
     handleSubmit = (e) => {
-        fetch(`https://api.github.com/users/${this.state.username}/repos`)
+        fetch(`https://api.github.com/users/${this.state.username}/repos`, {
+            headers: {
+                'Authorization': 'token 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'
+            },
+        })
         .then(res => res.json())
         .then(data => {
             console.log('data',data)
-            this.setState({repo:data})
+            this.setState({repos:data})
             this.setState({ username: e.target.value })
         })
         .catch(err => {
@@ -35,44 +37,33 @@ class GithubSearch extends Component {
     }
 
     render() {
-        console.log(this.state.repo)
+        console.log('render', this.state.repos)
         const {
             username,
             repos,
-            apiMsg,
-            userInfo: {
-                avatar_url,
-                login,
-                html_url
-            }
         } = this.state
 
         return (
             
             <Fragment>
                 <div className='header'>Github Search</div>
-                <form className='search'>
-                    <input 
-                        placeholder='Github user'
-                        name='github user' 
-                        type='text' 
-                        onChange={this.handleChange}
-                        value={username}
-                    />
-                    <button onClick={this.handleSubmit} >Search</button>
-                </form>
-                <p>{apiMsg}</p>
-
+                    <div className='search'>
+                        <input 
+                            placeholder='Github user'
+                            name='github user' 
+                            type='text' 
+                            onChange={this.handleChange}
+                            value={username}
+                        />
+                        <button onClick={this.handleSubmit} >Search</button>
+                    </div>
         {
             repos.length > 0 &&
                 <Fragment>
-                    <div className='user-info'>
-                        <img className='img-responsive center-block' src={avatar_url} alt='avatar'/>
-                        <h3>{login}</h3>
-                        <h4><a href={html_url} target='_blank' rel="noreferrer">{html_url}</a></h4>
-                    </div>
-                    <div className='right-container'>
-                        {this.state.repos.map(repo => <ListComponent key={repo.id} {...repo}/>)}
+                    <div className='list-container'>
+                        <div className='col'>
+                            {this.state.repos.map(repo => !repo.fork && <ListComponent key={repo.id} {...repo} />)}                        
+                        </div>
                     </div>
                 </Fragment>
         }
--- src/components/ListComponent.js
+++ src/components/ListComponent.js
@@ -5,13 +5,16 @@ const ListComponent = ({ name, description, language, html_url }) => {
     return (
         <div className='card'>
             <div className='card-body'>
-                <h2><a href={html_url} target='_blank' rel="noreferrer">{name}</a></h2>
-            </div>
-            <div className='card-body'>
-                <p>{description}</p>
-            </div>
-            <div className='card-body'>
-                <p>Languages: {language}</p>
+                <div>
+                    <h4><a href={html_url} target='_blank' rel="noreferrer">{name}</a></h4>
+                </div>
+                <hr/>
+                <div>
+                    <p>Description: {description}</p>
+                </div>
+                <div>
+                    <p>Languages: {language}</p>
+                </div>
             </div>
         </div>
     );
